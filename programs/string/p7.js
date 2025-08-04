f13450716bdc2aa77a8ef2a7111699b7c64a5c6b
Rohit Patil <patilrohit298@gmail.com>
2025-08-04 09:30:09 +0530

    Update VSCode settings to enable quick suggestions for other types; clean up p7.js by removing unnecessary whitespace and ensuring consistent formatting.

--- programs/string/p7.js
+++ programs/string/p7.js
@@ -3,7 +3,6 @@
 // Output: t,n
 
 
-
 function findDuplicate(str) {
     let map = {}
     let result = []
@@ -20,3 +19,26 @@ function findDuplicate(str) {
 
 console.log(findDuplicate('tenant'))
 
+function findDuplicate1(str){
+    let obj = {}
+    let result = []
+    for (let index = 0; index < str.length; index++) {
+        const element = str[index];
+        if(!obj[element]){
+            obj[element] = 1
+        }else{
+            obj[element] = obj[element] + 1
+            result.push(element)
+        }        
+    }
+    
+
+//    for(let [key,value] of Object.entries(obj)){
+//     if(value> 1){
+        
+//     }
+//    }
+   return result
+}
+
+console.log(findDuplicate1('tenant'))
